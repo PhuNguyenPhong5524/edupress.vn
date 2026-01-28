8fa70caf6edec1612c6508c4816fbe8d50270f75
nguyenphongphu1234 <134608320+PhuNguyenPhong5524@users.noreply.github.com>
2026-01-28 10:29:52 +0700

    Update Box Show cataegory HomePgae

--- src/pages/customer/Home/BoxShowCategory/SwiperCategory.jsx
+++ src/pages/customer/Home/BoxShowCategory/SwiperCategory.jsx
@@ -1,21 +1,27 @@
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
+import { Navigation,Grid } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/grid";
 import "swiper/css/navigation";
 import BoxCateGory from "./BoxCategory";
 import ChevronLeftIcon from "../../../../components/icons/ChevronLeftIcon";
 import ChevronRightIcon from "../../../../components/icons/ChevronRightIcon";
 
+const skeletonItems = Array.from({ length: 12 }, (_, i) => ({
+    id: `skeleton-${i}`,
+    isSkeleton: true,
+}));
 
 const SwiperCategory = ({ categories, loading }) => {
+    const renderData = loading ? skeletonItems : categories;
   return (
         <div className="relative">
             {/* Custom arrows */}
             <button 
                 className="
                     cate-prev absolute left-[-5px] lg:-left-5 top-1/2 -translate-y-1/2 z-10 border-[1px] border-[#E0E0E0]
-                    w-10 h-10 bg-white rounded-full shadow flex items-center justify-center 
+                    w-10 h-10 bg-white rounded-full shadow hidden lg:flex items-center justify-center 
                     hover:bg-[#FF782D] hover:text-white hover:opacity-60 transition cursor-pointer
                 "
             >
@@ -25,56 +31,62 @@ const SwiperCategory = ({ categories, loading }) => {
             <button 
                 className="
                 cate-next absolute right-[-5px] lg:-right-5 top-1/2 -translate-y-1/2 z-10 border-[1px] border-[#E0E0E0] 
-                w-10 h-10 bg-white rounded-full shadow flex items-center justify-center
+                w-10 h-10 bg-white rounded-full shadow hidden lg:flex items-center justify-center
                 hover:bg-[#FF782D] hover:text-white hover:opacity-60 transition cursor-pointer
                 "
             >
                 <ChevronRightIcon size={20} />
             </button>
 
-            <Swiper
-                modules={[Navigation]}
-                navigation={{
+         <Swiper
+            modules={[Navigation, Grid]}
+            navigation={{
                 prevEl: ".cate-prev",
                 nextEl: ".cate-next",
-                }}
-                spaceBetween={12}
-                slidesPerView={6}
-                slidesPerGroup={6}
-                grabCursor
-                breakpoints={{
+            }}
+            spaceBetween={12}
+            grabCursor
+            breakpoints={{
                 1024: {
-                    slidesPerView: 6,
-                    slidesPerGroup: 6,
+                slidesPerView: 6,
+                slidesPerGroup: 6,
+                grid: {
+                    rows: 1,
+                },
                 },
                 768: {
-                    slidesPerView: 4,
-                    slidesPerGroup: 4,
+                slidesPerView: 4.5,
+                slidesPerGroup: 4,
+                grid: {
+                    rows: 1,
+                },
                 },
                 480: {
-                    slidesPerView: 3,
-                    slidesPerGroup: 3,
+                slidesPerView: 3.5,
+                slidesPerGroup: 3,
+                grid: {
+                    rows: 1,
+                },
                 },
                 0: {
-                    slidesPerView: 2,
-                    slidesPerGroup: 2,
+                slidesPerView: 3.5,
+                slidesPerGroup: 2, // 🔥 QUAN TRỌNG
+                grid: {
+                    rows: 2,
+                    fill: "row",
                 },
-                }}
+                },
+            }}
             >
                 {
-                loading ? (
-                    <button className="cate-btn w-full h-[180px] bg-[#d8d8d8] text-[#000000] rounded-[20px] animate-pulse flex justify-center items-center">
-                        Loading...
-                    </button>
-                ) : (
-                    categories?.map((item) => (
+                    renderData.map((item) => (
                         <SwiperSlide key={item.id}>
                             <BoxCateGory item={item} />
                         </SwiperSlide>
                     ))
-                )}
-           
+                }
             </Swiper>
+
         </div>
     );
 }
